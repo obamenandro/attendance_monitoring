6426ee4f323d3c4d1cc45c152eb6dfa6fede741b
davetorrente <dave.torrente@gmail.com>
2017-12-21 13:09:47 +0800

    migrations

---
+++ config/Migrations/20171219075741_UserMigration.php
@@ -0,0 +1,87 @@
+<?php
+use Migrations\AbstractMigration;
+
+class UserMigration extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     * @return void
+     */
+    // public function change()
+    // {
+    // }
+    public function up()
+    {
+        $usersTable = $this->table('users');
+        $usersTable
+            ->addColumn('firstname', 'string', ['length' => 255])
+            ->addColumn('middlename', 'string', [
+                'default'=>null,
+                'length' => 255,
+                'null' => true
+            ])
+            ->addColumn('lastname', 'string', ['length' => 255])
+            ->addColumn('bday', 'string', ['length' => 255])
+            ->addColumn('address', 'string', ['length' => 255])
+            ->addColumn('contact', 'integer', ['length' => 11])
+            ->addColumn('email', 'string', ['length' => 255])
+            ->addColumn('place_of_birth', 'string', ['length' => 255])
+            ->addColumn('citizenship', 'string', ['length' => 255])
+            ->addColumn('civil_status', 'string', ['length' => 255])
+            ->addColumn('name_of_spouse', 'string', [
+                'default'=>null,
+                'length' => 255,
+                'null' => true
+            ])
+            ->addColumn('number_of_children', 'integer', [
+                'default'=>0,
+                'length' => 11,
+                'null' => true
+            ])
+            ->addColumn('government_id', 'integer', ['length' => 1])
+            ->addColumn('educational_attainment', 'string', ['length' => 255])
+            ->addColumn('eligibility', 'string', [
+                'default'=>null,
+                'length' => 255,
+                'null' => true
+            ])
+            ->addColumn('work_experience', 'string', [
+                'default'=>null,
+                'length' => 255,
+                'null' => true
+            ])
+            ->addColumn('trainings', 'string', [
+                'default'=>null,
+                'length' => 255,
+                'null' => true
+            ])
+            ->addColumn('jobtype', 'integer', [
+                'default'=>null,
+                'length' => 11,
+                'null' => true
+            ])
+            ->addColumn('destination_id', 'integer', [
+                'default'=>null,
+                'length' => 11,
+                'null' => true
+            ])
+            ->addColumn('created', 'datetime', [
+                'default' => 'CURRENT_TIMESTAMP',
+                'limit' => null,
+                'null' => true,
+            ])
+            ->addColumn('modified', 'datetime', [
+                'default' => 'CURRENT_TIMESTAMP',
+                'limit' => null,
+                'null' => true,
+            ])
+            ->create();
+    }
+    public function down()
+    {
+        $this->dropTable('users');
+    }
+}
