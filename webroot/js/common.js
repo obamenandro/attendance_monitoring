787701f3d2c49742c3d3759de91cda1b305adf3b
Daryll <darylldigo@gmail.com>
2018-01-27 17:47:25 +0800

    develop

--- webroot/js/common.js
+++ webroot/js/common.js
@@ -3,7 +3,6 @@ $(document).ready(function() {
   $('#dataTable').DataTable({
     "pagingType": "first_last_numbers",
     'bFilter': false,
-    "sDom": '<"top"flp>rt<"bottom"i><"clear">'
   });
 
 });
