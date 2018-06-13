4c1b098bed22a38b0c3ca948706c56f7b07c1947
obamenandro <obamenandro@gmail.com>
2018-06-13 08:44:44 +0800

    d

--- config/routes.php
+++ config/routes.php
@@ -49,6 +49,7 @@ Router::scope('/', function (RouteBuilder $routes) {
      * its action called 'display', and we pass a param to select the view file
      * to use (in this case, src/Template/Pages/home.ctp)...
      */
+    $routes->connect('/', ['controller' => 'Users', 'action' => 'login']);
     $routes->connect('/user/leave', ['controller' => 'Pages', 'action' => 'display', 'user_leave']);
     /**
      * ...and connect the rest of 'Pages' controller's URLs.
